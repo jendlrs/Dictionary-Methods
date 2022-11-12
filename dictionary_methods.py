509a605c83a96ccecfe6f00a7ac1365756d7795b
unknown <jensennics.dlrs@gmail.com>
2022-11-12 11:29:46 +0800

    I listed my initial steps for the program and finished the first step which is to display the Menu.

---
+++ dictionary_methods.py
@@ -0,0 +1,49 @@
+#Seatwork 3 in DSA
+
+#Write a python program for contact tracing:
+
+#- Display a menu of options
+#	Menu:
+#		 1 -> Add an item
+#		 2 -> Search
+#		 3 -> Exit (y/n)
+#- Allow user to select item in the menu (check if valid)
+#- Perform the selected option
+#		- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+#				   Use dictionary to store the info
+#				   Use the full name as key
+#				   The value is another dictionary of personal information
+#		- Option 2: Search, ask full name then display the record
+#		- Option 3: Ask the user if want to exit or retry.
+
+#Example Output:
+
+#Menu:
+# 1 -> Add an item
+# 2 -> Search
+# 3 -> Exit (y/n)
+
+#What do you want to do? (1-3): 1
+#Full name: Danilo Madrigalejos
+#Age: 30
+#Address: Eastwood
+#Phone number: 1234567890
+#Saved!
+#What do you want to do? (1-3): 2
+#Full name: Danilo Madrigalejos
+#Age: 30
+#Address: Eastwood
+#Phone number: 1234567890 What do you want to do? (1-3): 3
+#Exit? n
+
+#1st Step: Display the Menu
+print ("\n=======-MENU-=======\n")
+print ("  1 --> Add an Item")
+print ("  2 --> Search")
+print ("  3 --> Exit\n")
+print ("====================")
+#Ask Input from the user 
+#Add the condition
+    #Option1
+    #Option2
+    #Option3
