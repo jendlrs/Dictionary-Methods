4f34be40a1a04987e008680382df6d70fe939552
unknown <jensennics.dlrs@gmail.com>
2022-11-12 17:42:02 +0800

    I rearrange also my dictionary, my data 1 changed into the name. I also deleted my condition for option 2 because they are all wrong.

--- dictionary_methods.py
+++ dictionary_methods.py
@@ -42,30 +42,31 @@ print ("  1 --> Add an Item")
 print ("  2 --> Search")
 print ("  3 --> Exit\n")
 print ("====================")
-#2nd Step: Ask Input from the user 
+
+#dictionary
+personalDataDict = {
+    "Ma. Jensen Nicole C. Dela Rosa" : {
+        "Name" : "Ma. Jensen Nicole C. Dela Rosa",
+        "Age" : 19,
+        "Gender" : "F",
+        "Address" : "Quezon City",
+        "Phone Number" : "09238271212",
+        "Email Address" : "jensennics.dlrs@gmail.com",
+        "COVID 19 Vaccination Status" : "Fully Vaccinated"
+    },
+    "Placeholder2" : {
+        "Name" : "Person2",
+        "Age" : 1, #for int
+        "Gender" : "",
+        "Address" : "",
+        "Phone Number" : "",
+        "Email Address" : "",
+        "COVID 19 Vaccination Status" : ""
+    },
+}
 while True:
     userInputFunct = int(input ("\nWhat do you want to do? [Enter the corresponding number only \033[92m(1-3)\033[0m from the MENU]: "))
     #3rd Step: Declaring a Dictionary
-    personalDataDict = {
-                "data1" : {
-                    "Name" : "Ma. Jensen Nicole C. Dela Rosa",
-                    "Age" : 19,
-                    "Gender" : "F",
-                    "Address" : "Quezon City",
-                    "Phone Number" : "09238271212",
-                    "Email Address" : "jensennics.dlrs@gmail.com",
-                    "COVID 19 Vaccination Status" : "Fully Vaccinated"
-                },
-                "data2" : {
-                    "Name" : "Person2",
-                    "Age" : 1, #for int
-                    "Gender" : "",
-                    "Address" : "",
-                    "Phone Number" : "",
-                    "Email Address" : "",
-                    "COVID 19 Vaccination Status" : ""
-                },
-            }
     if userInputFunct not in range (1,4):
         print ("\n=====Sorry you have entered an invalid input.===== \n            Please Enter 1 to 3 only.")
     #4th Step: Add the condition
@@ -97,28 +98,12 @@ while True:
         #display updated Info
         #Option2
     elif userInputFunct == 2:
-        nameSearch = input ("Please enter your full name: ")
-                #for data 1
-        for key, value in personalDataDict["data1"].items():
-            if value == nameSearch:
-                print ("\nThese are what we found: \n")
-        #display updated Info
-                for key, value in personalDataDict["data1"].items():
-                    print(key, ":", value,"\n")
-        #for data 2
-            for key, value in personalDataDict["data2"].items():
-                if value == nameSearch:
-                    print ("\nThese are what we found: \n")
-        #display updated Info
-                    for key, value in personalDataDict["data2"].items():
-                        print(key, ":", value,"\n")
-        else: 
-            print("The name you entered did not match any of our records.")
+        print("option2")
         #option3
     elif userInputFunct == 3:
         exitVerification = input("Are you sure you want to exit? (Y/N) ")
         if exitVerification == "Y":
-            print ("\nThank you for using this program")
+            print ("\nThank you for using this program\n")
             break
         else:
             continue
