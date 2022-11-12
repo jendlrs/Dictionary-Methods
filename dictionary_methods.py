ce1965aa4ac8a3b8010e6a2828a5708a1fe35dc9
unknown <jensennics.dlrs@gmail.com>
2022-11-12 13:17:02 +0800

    I added While loop to verify the input from user. It will limit the user to enter invalid input specifically when it is not in range 1-4.

--- dictionary_methods.py
+++ dictionary_methods.py
@@ -43,49 +43,52 @@ print ("  2 --> Search")
 print ("  3 --> Exit\n")
 print ("====================")
 #2nd Step: Ask Input from the user 
-userInputFunct = int(input ("\nWhat do you want to do? [Enter the corresponding number only \033[92m(1-3)\033[0m from the MENU]: "))
-#3rd Step: Declaring a Dictionary
-personalDataDict = {
-    "Name" : "Ma. Jensen Nicole C. Dela Rosa",
-    "Age" : 19,
-    "Gender" : "F",
-    "Address" : "Quezon City",
-    "Phone Number" : "09238271212",
-    "Email Address" : "jensennics.dlrs@gmail.com",
-    "COVID 19 Vaccination Status" : "Fully Vaccinated"
-    }
-#4th Step: Add the condition
-    #Option1
-if userInputFunct == 1:
-    name = input ("What is your Name? ")
-    age = int (input("How old are you? "))
-    gender = input ("What is your Gender? (F/N) ")
-    address = input ("Where do you live? ")
-    phoneNumber = input ("What is your phone number? ")
-    emailAdd = input ("What is your email? ")
-    vaccineStatus = input ("What is your COVID 19 Vaccine Status? ")
+while True:
+    userInputFunct = int(input ("\nWhat do you want to do? [Enter the corresponding number only \033[92m(1-3)\033[0m from the MENU]: "))
+    #3rd Step: Declaring a Dictionary
+    if userInputFunct not in range (1,4):
+        print ("Sorry")
+        personalDataDict = {
+            "Name" : "Ma. Jensen Nicole C. Dela Rosa",
+            "Age" : 19,
+            "Gender" : "F",
+            "Address" : "Quezon City",
+            "Phone Number" : "09238271212",
+            "Email Address" : "jensennics.dlrs@gmail.com",
+            "COVID 19 Vaccination Status" : "Fully Vaccinated"
+            }
+    #4th Step: Add the condition
+        #Option1
+    elif userInputFunct == 1:
+        name = input ("What is your Name? ")
+        age = int (input("How old are you? "))
+        gender = input ("What is your Gender? (F/N) ")
+        address = input ("Where do you live? ")
+        phoneNumber = input ("What is your phone number? ")
+        emailAdd = input ("What is your email? ")
+        vaccineStatus = input ("What is your COVID 19 Vaccine Status? ")
 
-    #modifying existing item
-    personalDataDict ["Name"] = name
-    personalDataDict ["Age"] = age
-    personalDataDict ["Gender"] = gender
-    personalDataDict ["Address"] = address
-    personalDataDict ["Phone Number"] = phoneNumber
-    personalDataDict ["Email Address"] = emailAdd
-    personalDataDict ["COVID 19 Vaccination Status"] = vaccineStatus
+        #modifying existing item
+        personalDataDict ["Name"] = name
+        personalDataDict ["Age"] = age
+        personalDataDict ["Gender"] = gender
+        personalDataDict ["Address"] = address
+        personalDataDict ["Phone Number"] = phoneNumber
+        personalDataDict ["Email Address"] = emailAdd
+        personalDataDict ["COVID 19 Vaccination Status"] = vaccineStatus
 
-    print ("\n===-Personal Information is Saved-===\n")
-    #display updated Info
-    print ("\nThese are the information we received:\n")
-    for key, value in personalDataDict.items():
-        print(key, ":", value,"\n")
-    #Option2
-elif userInputFunct == 2:
-    name = input ("Please enter your full name: ")
-    if "Name" in personalDataDict:
-        print ("\nThese are what we found: \n")
+        print ("\n===-Personal Information is Saved-===\n")
+        #display updated Info
+        print ("\nThese are the information we received:\n")
         for key, value in personalDataDict.items():
             print(key, ":", value,"\n")
-    #option3
-elif userInputFunct == 3:
-    print("Option 3")
+        #Option2
+    elif userInputFunct == 2:
+        name = input ("Please enter your full name: ")
+        if "Name" in personalDataDict:
+            print ("\nThese are what we found: \n")
+            for key, value in personalDataDict.items():
+                print(key, ":", value,"\n")
+        #option3
+    elif userInputFunct == 3:
+        print("Option 3")
